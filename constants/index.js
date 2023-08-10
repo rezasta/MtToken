8b06ab35666af59286b8a6da4508fb50f2fd3d25
rezasta <110907611+rezasta@users.noreply.github.com>
2023-08-11 00:50:41 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x19f09bf9a8f439fa949e9587c925263dc2b0c45c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
